5e0499e94a2104e076edf6768099b22ad822951d
Illia Kazmin <kazmin1c@gmail.com>
2022-08-19 23:05:51 +0200

    add task 9 and 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  changeColorBtn: document.querySelector('.change-color'),
+  colorSpan: document.querySelector('.color'),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = newColor;
+  refs.colorSpan.textContent = newColor;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector('#controls').firstElementChild,
+  createCollectionDivBtn: document.querySelector('button[data-create]'),
+  destroyCollectionDivBtn: document.querySelector('button[data-destroy]'),
+  boxEl: document.querySelector('#boxes'),
+};
+
+function createBoxes(amount) {
+  let newDiv = '';
+  let widthHeigth = 0;
+  refs.boxEl.innerHTML = '';
+  for (let i = 1; i <= amount; i += 1) {
+    widthHeigth = i === 1 ? 30 : widthHeigth + 10;
+    newDiv += `<div
+                style="width: ${widthHeigth}px; height: ${widthHeigth}px;
+                background-color: ${getRandomHexColor()};" >
+               </div>`;
+  }
+  refs.boxEl.insertAdjacentHTML('beforeend', newDiv);
+}
+
+refs.createCollectionDivBtn.addEventListener('click', () => {
+  createBoxes(refs.inputEl.value);
+});
+
+function onClickDestroy() {
+  refs.boxEl.innerHTML = '';
+  refs.inputEl.value = '';
+}
+
+refs.destroyCollectionDivBtn.addEventListener('click', onClickDestroy);

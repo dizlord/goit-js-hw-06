6dc0ed4d5d1980b8d682f684839388ff6b9d2665
Illia Kazmin <kazmin1c@gmail.com>
2022-08-26 17:51:25 +0200

    add localStorage to keep fontSize btwn reload pg

--- js/task-07.js
+++ js/task-07.js
@@ -3,10 +3,15 @@ const refs = {
   textEl: document.querySelector('#text'),
 };
 
+if (localStorage.getItem('fontSize')) {
+  refs.inputFontSizeEl.value = localStorage.getItem('fontSize');
+}
+
 refs.textEl.style.fontSize = `${refs.inputFontSizeEl.value}px`;
 
 refs.inputFontSizeEl.addEventListener('input', onChangeFontSize);
 
 function onChangeFontSize(event) {
   refs.textEl.style.fontSize = `${event.currentTarget.value}px`;
+  localStorage.setItem('fontSize', event.currentTarget.value);
 }
